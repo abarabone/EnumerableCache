7bf7e35ebf57c8581c071d4bcdf62e3c659a2481
abarabone <abara00@live.jp>
2018-12-04 23:44:38 +0900

    struct enumerator どうすればいい？

---
+++ ConsoleApp1/EnumerableCache.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System;
+
+namespace Abss.UtilityOther
+{
+	// ＬＩＮＱオペレータオブジェクトを使いまわしたいと思ったとき、
+	//		static Func<IEnumerable<int>, IEnumerable<int>>
+	//			calcLengths = xs => xs
+	//				.Select( model => model.BoneLength * model.ModelInstanceCount )
+	//				;
+	// みたいなのを持っておけばいいかな？と思ったけど、これだと calcLengths( source ) とした時に
+	// はじめて Linq のオペレータオブジェクトが生成されるので、やっぱりキャッシュオブジェクトは必要。
+	
+	// List<T> のような、struct enumerator の場合はどうやって扱っていいのだろうか
+	// Linq でも結局ボクシングしてるのかな？
+	
+	/// <summary>
+	/// 列挙ソースのレイトバインディングを可能にする。
+	/// </summary>
+	//public struct LateBindEnumerable<T, TEnumerator>
+	//	where TEnumerator : struct, IEnumerator<T>, IDisposable, IEnumerator
+	//{
+
+	//	public IEnumerable<T> EnumerableSource;
+		
+
+	//	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	//	public TEnumerator GetEnumerator()
+	//	{
+	//		return (TEnumerator)EnumerableSource.GetEnumerator();
+	//	}
+	//}
+	public struct LateBindEnumerable<T> : IEnumerable<T>
+	{
+
+		public IEnumerable<T> EnumerableSource;
+		
+		
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		public IEnumerator<T> GetEnumerator()
+		{
+			return EnumerableSource.GetEnumerator();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return EnumerableSource.GetEnumerator();
+		}
+	}
+
+	/// <summary>
+	/// ＬＩＮＱオブジェクトをキャッシュする。
+	/// </summary>
+	public struct EnumerableCache<Tsrc, Tdst>
+	{
+
+		LateBindEnumerable<Tsrc>	enumerable;	// 列挙ソースを
+
+		IEnumerable<Tdst>			query;		// オペレータ
+		
+		/// <summary>
+		/// ソースを決定し、ＬＩＮＱクエリを実行する。
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public IEnumerable<Tdst> Query( IEnumerable<Tsrc> source )
+		{
+			this.enumerable.EnumerableSource = source;
+			return this.query;
+		}
+		
+		/// <summary>
+		/// オペレータを登録する。
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public EnumerableCache( Func<LateBindEnumerable<Tsrc>, IEnumerable<Tdst>> operatorExpressionOnLateBind )
+		{
+			this.enumerable = new LateBindEnumerable<Tsrc>();
+			this.query		= operatorExpressionOnLateBind( this.enumerable );
+		}
+	}
+
+	static public class LateBindEnumerableExtension
+	{
+		static public IEnumerable<Tdst> QueryWith<Tsrc, Tdst>
+			( this IEnumerable<Tsrc> enumerableSource, EnumerableCache<Tsrc, Tdst> enumerableCache )
+		{
+			return enumerableCache.Query( enumerableSource );
+		}
+	}
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Abss.UtilityOther;
+
 namespace ConsoleApp1
 {
 	class Program
 	{
 		static void Main( string[] args )
 		{
+			var q = new LateBindEnumerable<int>();
+
+			q.EnumerableSource = Enumerable.Range( 0, 10 );
+
+			foreach( var i in q ) Console.WriteLine( i );
+			Console.ReadKey();
+		}
+
+		void aaa()
+		{
+			var e = new List<int>().GetEnumerator();
 		}
 	}
 }
